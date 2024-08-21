90fa808de9ff9bd7521182e17b8b5d27fc059773
ParadiseHub <160505534+ParadiseHub@users.noreply.github.com>
2024-08-21 15:57:59 -0300

    Add files via upload

---
+++ CalculadouraJS/calculadora.js
@@ -0,0 +1,11 @@
+let n1 = 20; 
+let n2 = 15;
+
+console.log("Soma: " + (n1 + n2));
+console.log("Subtracao: " + (n1 - n2));
+console.log("Multiplicaçao: " + (n1 * n2));
+console.log("Divisão: " + (n1 / n2));
+n1++;
+console.log("Numero 1 apos incremento " + n1 );
+n2--;
+console.log("Numero 2 apos decremento " + n2 );
